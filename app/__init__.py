eff5f2c13aa0f8c29e3a17a80a15fc19ec64ea45
Luis Oliveira <gu00col@gmail.com>
2025-02-10 14:19:37 -0300

    correcao

--- app/__init__.py
+++ app/__init__.py
@@ -22,31 +22,9 @@ else:
     logger.error('ENV INVALIDA.')
     sys.exit(1)
 
-from app.funcoes.evolution_api import feet_all_instances, adiciona_numero_contatos, atualiza_fotos_contatos
-
-scheduler_lock = threading.Lock()
-
-def start_scheduler():
-    with scheduler_lock:
-        if not scheduler.running:
-            logger.info("Scheduler start")
-            # scheduler.add_job(atualiza_fotos_contatos, 'interval', minutes=5)  # Executa a cada 1 minuto
-            scheduler.start()
-            logger.info("Scheduler started successfully")
-
-# Configuração do APScheduler
-scheduler = BackgroundScheduler()
 
 app = FastAPI()
 
-@app.on_event("startup")
-def on_startup():
-    start_scheduler()
-
-@app.on_event("shutdown")
-def shutdown_event():
-    scheduler.shutdown()
-    logger.info("Scheduler shut down successfully")
 
 from app.rotas.webhook import router as webhook
 app.include_router(webhook)
